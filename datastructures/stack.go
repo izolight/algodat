058c9ca22694577395e05c2523e1ee8f1dfaafe3
Gabor Tanz <gabor.tanz@gmail.com>
2017-05-27 13:30:45 +0200

    implement viewing of stack

--- datastructures/stack.go
+++ datastructures/stack.go
@@ -7,46 +7,68 @@ import (
 // Stack has the a pointer to the top element in the Stack, the current size and the maximum allowed size
 type Stack struct {
 	top     *Element
-	size    int
+	Size    int
 	maxSize int
 }
 
 // Element consists of the value and a pointer to the next Element in the stack
 type Element struct {
-	value int
+	Value int
 	next  *Element
 }
 
-func (s *Stack) push(val int) error {
+// Creates a new stack with the specified maxSize
+func NewStack(maxSize int) *Stack {
+	return &Stack{nil, 0, maxSize}
+}
+
+func (s *Stack) Push(val int) error {
 	if s.isFull() {
-		return fmt.Errorf("Can't push to full stack. Size: %d", s.size)
+		return fmt.Errorf("Can't push to full stack. Size: %d", s.Size)
 	}
 	top := Element{val, s.top}
 	s.top = &top
-	s.size++
+	s.Size++
 	return nil
 }
 
-func (s *Stack) pop() (int, error) {
+func (s *Stack) Pop() (int, error) {
 	if !s.isEmpty() {
-		val := s.top.value
+		val := s.top.Value
 		s.top = s.top.next
-		s.size--
+		s.Size--
 		return val, nil
 	}
 	return 0, fmt.Errorf("Can't pop from empty stack")
 }
 
+// Checks whether the stack is empty
 func (s *Stack) isEmpty() bool {
-	if s.size > 0 {
+	if s.Size > 0 {
 		return false
 	}
 	return true
 }
 
 func (s *Stack) isFull() bool {
-	if s.size == s.maxSize {
+	if s.Size == s.maxSize {
 		return true
 	}
 	return false
+}
+
+// Returns the top element if any
+func (s *Stack) Top() (*Element, error) {
+	if s.isEmpty() {
+		return nil, fmt.Errorf("Can't return top of empty stack")
+	}
+	return s.top, nil
+}
+
+// Returns the next element if any
+func (e *Element) Next() (*Element, error) {
+	if e.next != nil {
+		return e.next, nil
+	}
+	return nil, fmt.Errorf("No more nodes")
 }
--- main.go
+++ main.go
@@ -6,9 +6,16 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"gitea.izolight.xyz/gabor/algodat/datastructures"
 )
 
+var stack = datastructures.NewStack(20)
+
 func main() {
+	stack.Push(1)
+	stack.Push(9)
+	stack.Push(-3)
+	stack.Push(5)
 	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/", Index)
 	r.HandleFunc("/stack", ViewStack)
@@ -17,14 +24,28 @@ func main() {
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	datastructures := []string{"stack", "queue"}
-	for _, d := range datastructures {
+	ds := []string{"stack", "queue"}
+	for _, d := range ds {
 		fmt.Fprintf(w, "<a href='/%s'>%s</a><br>", d, d)
 	}
 }
 
 func ViewStack(w http.ResponseWriter, r *http.Request) {
-	//stack := new(Stack)
+	element, err := stack.Top()
+	if err != nil {
+		return
+	}
+	s := stack.Size -1
+	fmt.Fprint(w, "<table><caption>Stack</caption><tr><th>Position</th><th>Value</th><tr>")
+	for {
+		fmt.Fprintf(w, "<tr><td>%d</td><td>%d</td></tr>", s, element.Value)
+		element, err = element.Next()
+		if err != nil {
+			break
+		}
+		s--
+	}
+	fmt.Fprint(w, "</table>")
 }
 
 func ViewQueue(w http.ResponseWriter, r *http.Request) {
