7bb6664afe235abd0b610ab8a5930900a88b758f
Gabor Tanz <gabor.tanz@gmail.com>
2017-05-28 00:21:05 +0200

    put logic into stack file

--- datastructures/stack.go
+++ datastructures/stack.go
@@ -2,22 +2,35 @@ package datastructures
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
 )
+
+var templates = template.Must(template.ParseFiles("templates/header.html", "templates/footer.html", "templates/viewstack.html"))
 
 // Stack has the a pointer to the top element in the Stack, the current size and the maximum allowed size
 type Stack struct {
-	top     *Element
+	top     *element
 	Size    int
 	maxSize int
 }
 
-// Element consists of the value and a pointer to the next Element in the stack
-type Element struct {
+// element consists of the value and a pointer to the next element in the stack
+type element struct {
 	Value int
-	next  *Element
+	next  *element
 }
 
-// Creates a new stack with the specified maxSize
+type viewData struct {
+	Title string
+	Data  interface{}
+}
+
+type stackData struct {
+	Position, Value int
+}
+
+// NewStack creates a new stack with the specified maxSize
 func NewStack(maxSize int) *Stack {
 	return &Stack{nil, 0, maxSize}
 }
@@ -26,7 +39,7 @@ func (s *Stack) Push(val int) error {
 	if s.isFull() {
 		return fmt.Errorf("Can't push to full stack. Size: %d", s.Size)
 	}
-	top := Element{val, s.top}
+	top := element{val, s.top}
 	s.top = &top
 	s.Size++
 	return nil
@@ -58,7 +71,7 @@ func (s *Stack) isFull() bool {
 }
 
 // Returns the top element if any
-func (s *Stack) Top() (*Element, error) {
+func (s *Stack) getTop() (*element, error) {
 	if s.isEmpty() {
 		return nil, fmt.Errorf("Can't return top of empty stack")
 	}
@@ -66,9 +79,31 @@ func (s *Stack) Top() (*Element, error) {
 }
 
 // Returns the next element if any
-func (e *Element) Next() (*Element, error) {
+func (e *element) Next() (*element, error) {
 	if e.next != nil {
 		return e.next, nil
 	}
 	return nil, fmt.Errorf("No more nodes")
+}
+
+func (s *Stack) GetAll(w http.ResponseWriter, r *http.Request) {
+	e, err := s.getTop()
+	if err != nil {
+		fmt.Fprintf(w, "Could not get top of stack: %v", err)
+	}
+	var d []stackData
+	pos := s.Size
+	for {
+		d = append(d, stackData{pos, e.Value})
+		e, err = e.Next()
+		if err != nil {
+			break
+		}
+		pos--
+	}
+	data := viewData{"Stack", d}
+	err = templates.ExecuteTemplate(w, "viewstack", data)
+	if err != nil {
+		fmt.Fprintf(w, "Could not render template: %v", err)
+	}
 }
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 
@@ -11,16 +10,6 @@ import (
 )
 
 var stack = datastructures.NewStack(20)
-var templates = template.Must(template.ParseFiles("templates/header.html", "templates/footer.html", "templates/viewstack.html"))
-
-type viewData struct {
-	Title string
-	Data  interface{}
-}
-type stackData struct {
-	Position int
-	Value    int
-}
 
 func main() {
 	stack.Push(1)
@@ -29,7 +18,7 @@ func main() {
 	stack.Push(5)
 	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/", Index)
-	r.HandleFunc("/stack", ViewStack)
+	r.HandleFunc("/stack", stack.GetAll)
 	r.HandleFunc("queue", ViewQueue)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
@@ -38,28 +27,6 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	ds := []string{"stack", "queue"}
 	for _, d := range ds {
 		fmt.Fprintf(w, "<a href='/%s'>%s</a><br>", d, d)
-	}
-}
-
-func ViewStack(w http.ResponseWriter, r *http.Request) {
-	element, err := stack.Top()
-	if err != nil {
-		fmt.Fprintf(w, "Could not get top of stack: %v", err)
-	}
-	var values []stackData
-	s := stack.Size - 1
-	for {
-		values = append(values, stackData{s, element.Value})
-		element, err = element.Next()
-		if err != nil {
-			break
-		}
-		s--
-	}
-	data := viewData{"Stack", values}
-	err = templates.ExecuteTemplate(w, "viewstack", data)
-	if err != nil {
-		fmt.Fprintf(w, "Could not render template: %v", err)
 	}
 }
 
