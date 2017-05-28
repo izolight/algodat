65b769d5111b6d4d133194e8ee3b3c29374ebaa2
Gabor Tanz <gabor.tanz@gmail.com>
2017-05-28 15:19:03 +0200

    implement some linked list operations

--- datastructures/linkedList.go
+++ datastructures/linkedList.go
@@ -9,11 +9,42 @@ type Node struct {
 	next  *Node
 }
 
-func (l *LinkedList) insert(val int) error {
+func NewLinkedList() *LinkedList {
+	return &LinkedList{nil}
+}
+
+func (l *LinkedList) insertAtFront(val int) error {
+	node := Node{val, l.head}
+	l.head = &node
+	return nil
+}
+
+func (l *LinkedList) insertAtEnd(val int) error {
+	node := Node{val, nil}
+	if l.head == nil {
+		return l.insertAtFront(val)
+	}
+	previous, current := l.head, l.head
+	for current != nil {
+		previous = current
+		current = current.next
+	}
+	previous.next = &node
 	return nil
 }
 
 func (l *LinkedList) deleteFromFront() error {
+	l.head = l.head.next
+	return nil
+}
+
+func (l *LinkedList) deleteFromEnd() error {
+	previous, current := l.head, l.head
+	for current.next != nil {
+		previous = current
+		current = current.next
+	}
+	previous.next = nil
 	return nil
 }
 
