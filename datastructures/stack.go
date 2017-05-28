a4b9f98afbbec69ce63ac9968ca59b5fb72c08b8
Gabor Tanz <gabor.tanz@gmail.com>
2017-05-28 13:58:30 +0200

    implement stack with slice instead of pointers

--- datastructures/stack.go
+++ datastructures/stack.go
@@ -16,26 +16,15 @@ type viewData struct {
 	Errors []string
 }
 
-type stackData struct {
-	Position, Value int
-}
-
 // Stack has the a pointer to the top element in the Stack, the current size and the maximum allowed size
 type Stack struct {
-	top     *stackElement
-	size    int
-	maxSize int
-}
-
-// stackElement consists of the value and a pointer to the next stackElement in the stack
-type stackElement struct {
-	Value int
-	next  *stackElement
+	size, maxSize int
+	elements      []int
 }
 
 // NewStack creates a new stack with the specified maxSize
 func NewStack(maxSize int) *Stack {
-	return &Stack{nil, 0, maxSize}
+	return &Stack{0, maxSize, nil}
 }
 
 // Pushes element on the stack
@@ -43,29 +32,27 @@ func (s *Stack) push(val int) error {
 	if s.isFull() {
 		return fmt.Errorf("Can't push to full stack. Size: %d", s.size)
 	}
-	top := stackElement{val, s.top}
-	s.top = &top
+	s.elements = append(s.elements, val)
 	s.size++
 	return nil
 }
 
 // Pops element from the stack and returns it
-func (s *Stack) pop() (int, error) {
-	if !s.isEmpty() {
-		val := s.top.Value
-		s.top = s.top.next
-		s.size--
-		return val, nil
+func (s *Stack) pop() error {
+	if s.isEmpty() {
+		return fmt.Errorf("Can't pop from empty stack")
 	}
-	return 0, fmt.Errorf("Can't pop from empty stack")
+	s.elements = s.elements[:len(s.elements)-1]
+	s.size--
+	return nil
 }
 
 // Checks whether the stack is empty
 func (s *Stack) isEmpty() bool {
-	if s.size > 0 {
-		return false
+	if s.size == 0 {
+		return true
 	}
-	return true
+	return false
 }
 
 func (s *Stack) isFull() bool {
@@ -76,40 +63,17 @@ func (s *Stack) isFull() bool {
 }
 
 // Returns the top element if any
-func (s *Stack) peek() (*stackElement, error) {
+func (s *Stack) peek() (int, error) {
 	if s.isEmpty() {
-		return nil, fmt.Errorf("Can't return top of empty stack")
+		return 0, fmt.Errorf("Can't return top of empty stack")
 	}
-	return s.top, nil
-}
-
-// Returns the next element if any
-func (e *stackElement) Next() (*stackElement, error) {
-	if e.next != nil {
-		return e.next, nil
-	}
-	return nil, fmt.Errorf("No more nodes")
+	return s.elements[len(s.elements)-1], nil
 }
 
 // View displays all values on the stack
 func (s *Stack) View(w http.ResponseWriter, r *http.Request) {
-	var d []stackData
-	e, err := s.peek()
-	if err != nil {
-		errors = append(errors, fmt.Sprintf("Could not get top of stack: %v", err))
-	} else {
-		pos := s.size
-		for {
-			d = append(d, stackData{pos, e.Value})
-			e, err = e.Next()
-			if err != nil {
-				break
-			}
-			pos--
-		}
-	}
-	data := viewData{"Stack", d, errors}
-	err = stackTemplates.ExecuteTemplate(w, "viewstack", data)
+	data := viewData{"Stack", s.elements, errors}
+	err := stackTemplates.ExecuteTemplate(w, "viewstack", data)
 	if err != nil {
 		fmt.Fprintf(w, "Could not render template: %v", err)
 	}
@@ -132,7 +96,7 @@ func (s *Stack) Add(w http.ResponseWriter, r *http.Request) {
 
 // Remove pops the top value from the stack
 func (s *Stack) Remove(w http.ResponseWriter, r *http.Request) {
-	_, err := s.pop()
+	err := s.pop()
 	if err != nil {
 		errors = append(errors, fmt.Sprintf("Could not pop from stack: %v", err))
 	}
