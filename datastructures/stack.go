e30177c7763739e56a8f1a6d0f3e00046d22c499
Gabor Tanz <gabor.tanz@gmail.com>
2017-06-03 09:00:31 +0200

    try to make stack more generic

--- datastructures/stack.go
+++ datastructures/stack.go
@@ -2,116 +2,57 @@ package datastructures
 
 import (
 	"fmt"
-	"html/template"
-	"net/http"
-	"strconv"
 )
-
-var stackTemplates = template.Must(template.ParseFiles("templates/header.html", "templates/footer.html", "templates/stack.html"))
 
 // Stack has the a pointer to the top element in the Stack, the current size and the maximum allowed size
 type Stack struct {
-	size, maxSize int
-	elements      []int
-	messages      []string
-	errors        []string
-}
-
-type stackData struct {
-	Elements []int
-	Peek     string
+	maxSize  int
+	elements []fmt.Stringer
 }
 
 // NewStack creates a new stack with the specified maxSize
 func NewStack(maxSize int) *Stack {
-	return &Stack{0, maxSize, nil, nil, nil}
+	return &Stack{maxSize, nil}
 }
 
 // Pushes element on the stack
-func (s *Stack) push(val int) error {
+func (s *Stack) push(val fmt.Stringer) error {
 	if s.isFull() {
-		return fmt.Errorf("Can't push to full stack. Size: %d", s.size)
+		return fmt.Errorf("Can't push to full stack. Size: %d", len(s.elements))
 	}
 	s.elements = append(s.elements, val)
-	s.size++
 	return nil
 }
 
 // Pops element from the stack and returns it
-func (s *Stack) pop() (int, error) {
+func (s *Stack) pop() (string, error) {
 	if s.isEmpty() {
-		return 0, fmt.Errorf("Can't pop from empty stack")
+		return "", fmt.Errorf("Can't pop from empty stack")
 	}
 	popped := s.elements[len(s.elements)-1]
 	s.elements = s.elements[:len(s.elements)-1]
-	s.size--
-	return popped, nil
+	return popped.String(), nil
 }
 
 // Checks whether the stack is empty
 func (s *Stack) isEmpty() bool {
-	if s.size == 0 {
+	if len(s.elements) == 0 {
 		return true
 	}
 	return false
 }
 
 func (s *Stack) isFull() bool {
-	if s.size == s.maxSize {
+	if len(s.elements) == s.maxSize {
 		return true
 	}
 	return false
 }
 
 // Returns the top element if any
-func (s *Stack) peek() (int, error) {
+func (s *Stack) peek() (string, error) {
 	if s.isEmpty() {
-		return 0, fmt.Errorf("Can't return top of empty stack")
+		return "", fmt.Errorf("Can't peek into empty stack")
 	}
-	return s.elements[len(s.elements)-1], nil
-}
-
-// View displays all values on the stack
-func (s *Stack) View(w http.ResponseWriter, r *http.Request) {
-	data := stackData{s.elements, ""}
-	peek, err := s.peek()
-	if err != nil {
-		s.errors = append(s.errors, fmt.Sprintf("%v", err))
-	} else {
-		data.Peek = fmt.Sprintf("%d at position %d", peek, s.size-1)
-	}
-	view := viewData{"Stack", data, s.messages, s.errors}
-	err = stackTemplates.ExecuteTemplate(w, "stack", view)
-	if err != nil {
-		fmt.Fprintf(w, "Could not render template: %v", err)
-	}
-	s.messages = s.messages[:0]
-	s.errors = s.errors[:0]
-}
-
-// Push takes a value from a form and pushes it on the stack
-func (s *Stack) Push(w http.ResponseWriter, r *http.Request) {
-	new, err := strconv.Atoi(r.FormValue("new"))
-	if err != nil {
-		s.errors = append(s.errors, fmt.Sprintf("Could not parse form value: %v", err))
-	} else {
-		err = s.push(new)
-		if err != nil {
-			s.errors = append(s.errors, fmt.Sprintf("%v", err))
-		} else {
-			s.messages = append(s.messages, fmt.Sprintf("Pushed %d to stack", new))
-		}
-	}
-	http.Redirect(w, r, "/stack", http.StatusSeeOther)
-}
-
-// Pop pops the top value from the stack
-func (s *Stack) Pop(w http.ResponseWriter, r *http.Request) {
-	popped, err := s.pop()
-	if err != nil {
-		s.errors = append(s.errors, fmt.Sprintf("%v", err))
-	} else {
-		s.messages = append(s.messages, fmt.Sprintf("Popped %d from stack", popped))
-	}
-	http.Redirect(w, r, "/stack", http.StatusSeeOther)
+	return s.elements[len(s.elements)-1].String(), nil
 }
---
+++ handlers/stackHandler.go
@@ -0,0 +1,64 @@
+package handlers
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+
+	"gitea.izolight.xyz/gabor/algodat/datastructures"
+)
+
+var stackTemplates = template.Must(template.ParseFiles("templates/header.html", "templates/footer.html", "templates/stack.html"))
+
+type StackModel struct {
+	Messages []string
+	Errors   []string
+	Stack    datastructures.Stack
+	Peek     string
+}
+
+// View displays all values on the stack
+func (s *Stack) View(w http.ResponseWriter, r *http.Request) {
+	data := stackData{s.elements, ""}
+	peek, err := s.peek()
+	if err != nil {
+		s.errors = append(s.errors, fmt.Sprintf("%v", err))
+	} else {
+		data.Peek = fmt.Sprintf("%d at position %d", peek, s.size-1)
+	}
+	view := viewData{"Stack", data, s.messages, s.errors}
+	err = stackTemplates.ExecuteTemplate(w, "stack", view)
+	if err != nil {
+		fmt.Fprintf(w, "Could not render template: %v", err)
+	}
+	s.messages = s.messages[:0]
+	s.errors = s.errors[:0]
+}
+
+// Push takes a value from a form and pushes it on the stack
+func (s *Stack) Push(w http.ResponseWriter, r *http.Request) {
+	new, err := strconv.Atoi(r.FormValue("new"))
+	if err != nil {
+		s.errors = append(s.errors, fmt.Sprintf("Could not parse form value: %v", err))
+	} else {
+		err = s.push(new)
+		if err != nil {
+			s.errors = append(s.errors, fmt.Sprintf("%v", err))
+		} else {
+			s.messages = append(s.messages, fmt.Sprintf("Pushed %d to stack", new))
+		}
+	}
+	http.Redirect(w, r, "/stack", http.StatusSeeOther)
+}
+
+// Pop pops the top value from the stack
+func (s *Stack) Pop(w http.ResponseWriter, r *http.Request) {
+	popped, err := s.pop()
+	if err != nil {
+		s.errors = append(s.errors, fmt.Sprintf("%v", err))
+	} else {
+		s.messages = append(s.messages, fmt.Sprintf("Popped %d from stack", popped))
+	}
+	http.Redirect(w, r, "/stack", http.StatusSeeOther)
+}
